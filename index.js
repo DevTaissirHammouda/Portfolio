2ec1a800e28a49cdd9b5c78d98bcc7158d686c23
kaizoku <taissir7ammouda@gmail.com>
2023-06-08 02:28:39 +0100

    some fixing

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+  if (window.scrollY>350) {
     document.getElementById("nav").className = "vnav navbar navbar-expand-sm    fixed-top";
   }
-  else if (document.body.scrollTop ==0 || document.documentElement.scrollTop ==0) {
+  else {
     document.getElementById("nav").className = "navbar navbar-expand-sm bg-dark  navbar-dark fixed-top";
   }
 }
